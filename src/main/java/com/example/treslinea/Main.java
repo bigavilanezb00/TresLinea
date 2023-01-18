484ec27849e8dc1cf8744d2c89e52d596b85b05f
bryan <bigavilanezb00@elpuig.xeill.net>
2023-01-18 23:07:21 +0100

    Terminado

---
+++ src/main/java/com/example/treslinea/Main.java
@@ -0,0 +1,25 @@
+package com.example.treslinea;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("TresLinea.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        primaryStage.setTitle("Tres en raya");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
